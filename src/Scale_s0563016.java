9bc51b66cfc22a7ce5d5404afdb421524ef4f4bb
inkompotato <fra.js.1998@gmail.com>
2018-07-16 16:31:02 +0200

    added Kaes3kuch3n's solution for bilinear interpolation

--- src/Scale_s0563016.java
+++ src/Scale_s0563016.java
@@ -65,40 +65,92 @@ public class Scale_s0563016 implements PlugInFilter {
         int[] pix = (int[]) ip.getPixels();
         int[] pix_n = (int[]) ip_n.getPixels();
 
-        float h = (float)height/(float)height_n;
-        float v = (float)width/(float)width_n;
+        float scaleY = (float)height/(float)height_n;
+        float scaleX = (float)width/(float)width_n;
 
-        // Schleife ueber das neue Bild
+        float scaleY1 = (float)height_n/(float)height;
+        float scaleX1 = (float)width_n/(float)width;
+
+        // Schleife ueber das neue Bildefrgrrthththghzhz
         for (int y_n = 0; y_n < height_n; y_n++) {
             for (int x_n = 0; x_n < width_n; x_n++) {
 
-                if (choice.equals("Kopie")) {
+                switch (choice) {
+                    case "Kopie": {
 
-                    int y = y_n;
-                    int x = x_n;
+                        int y = y_n;
+                        int x = x_n;
 
-                    if (y < height && x < width) {
-                        int pos_n = y_n * width_n + x_n;
-                        int pos = y * width + x;
+                        if (y < height && x < width) {
+                            int pos_n = y_n * width_n + x_n;
+                            int pos = y * width + x;
 
-                        pix_n[pos_n] = pix[pos];
+                            pix_n[pos_n] = pix[pos];
+                        }
+                        break;
                     }
-                }
-                else if (choice.equals("Pixelwiederholung")) {
+                    case "Pixelwiederholung": {
 
-                    int x = Math.round(x_n*v);
-                    int y = Math.round(y_n*h);
+                        int x = Math.round(x_n * scaleX);
+                        int y = Math.round(y_n * scaleY);
 
-                    if (y < height && x < width) {
-                        int pos_n = y_n * width_n + x_n;
-                        int pos = y * width + x;
+                        if (y < height && x < width) {
+                            int pos_n = y_n * width_n + x_n;
+                            int pos = y * width + x;
 
-                        pix_n[pos_n] = pix[pos];
+                            pix_n[pos_n] = pix[pos];
+                        }
+
+                        break;
                     }
+                    case "Bilinear":
+                        int pos_n = y_n * width_n + x_n;
 
+                        float x = x_n / scaleX1;
+                        float y = y_n / scaleY1;
+
+                        float xDistance = x - (int) x;
+                        float yDistance = y - (int) y;
+
+                        int[][] neighbors = new int[4][3];
+
+                        //Get neighbors
+                        for (int i = 0; i < 4; i++) {
+                            int xCoord = (int) x;
+                            int yCoord = (int) y;
+
+                            switch (i) {
+                                case 3:
+                                    xCoord = (xCoord + 1 < width) ? xCoord + 1 : width - 1;
+                                case 2:
+                                    yCoord = (yCoord + 1 < height) ? yCoord + 1 : height - 1;
+                                    break;
+                                case 1:
+                                    xCoord = (xCoord + 1 < width) ? xCoord + 1 : width - 1;
+                            }
+
+                            int pos = yCoord * width + xCoord;
+
+                            int color = pix[pos];
+                            neighbors[i][0] = (color & 0xff0000) >> 16;
+                            neighbors[i][1] = (color & 0xff00) >> 8;
+                            neighbors[i][2] = (color & 0xff);
+                        }
+
+                        //Calculate new color
+                        int r_n = (int) (neighbors[0][0] * (1 - xDistance) * (1 - yDistance) + neighbors[1][0] * xDistance * (1 - yDistance) + neighbors[2][0] * (1 - xDistance) * yDistance + neighbors[3][0] * xDistance * yDistance);
+                        int g_n = (int) (neighbors[0][1] * (1 - xDistance) * (1 - yDistance) + neighbors[1][1] * xDistance * (1 - yDistance) + neighbors[2][1] * (1 - xDistance) * yDistance + neighbors[3][1] * xDistance * yDistance);
+                        int b_n = (int) (neighbors[0][2] * (1 - xDistance) * (1 - yDistance) + neighbors[1][2] * xDistance * (1 - yDistance) + neighbors[2][2] * (1 - xDistance) * yDistance + neighbors[3][2] * xDistance * yDistance);
+
+                        pix_n[pos_n] = 0xff000000 | r_n << 16 | g_n << 8 | b_n;
+                        break;
                 }
+
+
             }
-        }
+                }
+
+
 
 
         // neues Bild anzeigen
